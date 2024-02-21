928e7ec7b00bc33ee7f0d497961393c028f40971
Gabriella Mansur <gabriellamesquita314@gmail.com>
2024-02-21 18:05:47 -0300

    rota locations e locations.html adicionados

--- app.py
+++ app.py
@@ -21,6 +21,15 @@ def get_profile(id):
 
     return render_template("profile.html", profile=profile_dict)
 
+@app.route("/locations")
+def get_list_locations():
+    url = "https://rickandmortyapi.com/api/location"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    locations_dict = json.loads(data)
+
+    return render_template("locations.html", locations=locations_dict['results'])
+
 @app.route("/lista")
 def get_list_characters():
     url = "https://rickandmortyapi.com/api/character"
